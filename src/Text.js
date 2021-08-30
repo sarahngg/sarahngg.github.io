57ca9a961b4e3933ddd4ecd62e088ac4da1137b2
Sarah Ng <sarah.ng26@gmail.com>
2021-08-29 18:03:38 -0700

    added styles to Text

--- src/Text.js
+++ src/Text.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Typography from '@material-ui/core/Typography';
 
 function Text(props) {
+  if (!props.value) return;
   let textElement = "";
   switch (props.type) {
     case "title":
@@ -14,6 +15,34 @@ function Text(props) {
     case "description":
       textElement = (
         <Typography variant="h5" component="h1" gutterBottom>
+          {props.value}
+        </Typography>
+      )
+      break;
+    case "card-title":
+      textElement = (
+        <Typography gutterBottom variant="h5" component="div">
+          {props.value}
+        </Typography>
+      )
+      break;
+    case "card-description":
+      textElement = (
+        <Typography variant="body1" color="text.secondary">
+          {props.value}
+        </Typography>
+      )
+      break;
+    case "modal-title":
+      textElement = (
+        <Typography gutterBottom variant="h6" component="h2">
+          {props.value}
+        </Typography>
+      )
+      break;
+    case "modal-description":
+      textElement = (
+        <Typography color="text.secondary" sx={{ mt: 2 }}>
           {props.value}
         </Typography>
       )

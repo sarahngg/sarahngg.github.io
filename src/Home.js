179ad442682c6f4e17caefcc2dbe2309e3588804
Sarah Ng <sarah.ng26@gmail.com>
2021-09-06 16:33:27 -0700

    used hooks and axios to populate site with server data

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,72 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Section from './components/Section';
 
-
 function Home(props) {
-  const sections = [
-    <Section title="Sarah Ng" description="👋 Hello" />,
-    <Section title="Projects" description="Things I did" 
-    contentList={props.projectList}/>
-  ];
+  const [user, setUser] = useState();
+  const [name, setName] = useState("");
+  const [tagline, setTagline] = useState("");
+  const [sections, setSections] = useState([]);
 
-  function addPropsToSections (sections) {
-    return sections.map((section, index) => {
-      return React.cloneElement(section, {
-        id: index,
-        key: index
+  const getUser = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/api/users/sn`);
+      const { user } = response.data;
+      setUser(user);
+      setName(user.name);
+      setTagline(user.tagline);
+      document.title = user.siteTitle;
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const getSections = async (sections) => {
+    try {
+      sections.forEach(async (sectionId, index ) => {
+        const response = await axios.get(`http://localhost:5000/api/sections/${sectionId}`);
+        const { section } = response.data;
+        setSections(sections => [...sections, section]);
       })
-    })
-  }
-  const sectionsWithIdKey = addPropsToSections(sections);
-  return sectionsWithIdKey;
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  
+  useEffect(() => {    
+    getUser();
+  }, []);
+
+  useEffect(() => {    
+    if (user)
+    getSections(user.sections);
+  }, [user]);
+
+  // const sections = [
+  //   <Section title={name} description={tagline} id="0" key="0"/>,
+  //   <Section title="Projects" description="Things I did" 
+  //   contentList={props.projectList}/>
+  // ];
+
+  // function addPropsToSections (sections) {
+  //   return sections.map((section, index) => {
+  //     console.log("addPropsToSections")
+  //     return React.cloneElement(section, {
+  //       id: index,
+  //       key: index
+  //     })
+  //   })
+  // }
+  const sectionsWithProps = sections.length ? 
+    sections.map((section, index) => 
+      <Section title={section.name} description={section.description} id={section._id} order={index + 1} key={section.name}/>)
+      : undefined;
+  return (<>
+  <Section title={name} description={tagline} order="0"/>
+  {sectionsWithProps}
+    </>)
+  // </>)
 }
 
 export default Home;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Preview from './Preview';
 import Text from './Text';
 import { Link } from 'react-router-dom';
@@ -10,26 +12,43 @@ const bgcolors = [
   'secondary.light'
 ]
 
-function getColor(id) {
-  return bgcolors[id%bgcolors.length];
+function getColor(index) {
+  return bgcolors[index%bgcolors.length];
 }
 
 function Section (props) {
+  const [items, setItems] = useState([]);
+  const getItems = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/api/items?sectionId=${props.id}`);
+      const { items } = response.data;
+      setItems(items);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {  
+    if (props.id) {
+      getItems();
+    }
+  }, [props.id]);
+
   return (
-    <Grid container bgcolor={getColor(props.id)} py={3}>
+    <Grid container bgcolor={getColor(props.order)} py={3}>
       <Grid item xs={1} md={2}/>
       <Grid item xs={10} md={8} >
       <Container>
         <Text type="title" value={props.title}/>
         <Text type="description" value={props.description}/>
-        {props.contentList && (<Grid container py={2} spacing={2}>
-          {props.contentList.map( content => 
-          <Grid item xs={12} md={4} key={content.id} >
-          <Link to={`/p/${content.id}`} style={{ textDecoration: 'none' }}>
+        {items.filter(item => item).length ? (<Grid container py={2} spacing={2} >
+          {items.map( content => 
+          <Grid item xs={12} md={4} key={content._id}>
+          <Link to={`/p/${content._id}`} style={{ textDecoration: 'none' }}>
           <Preview {... content}/>
           </Link>
           </Grid>)}
-        </Grid>)}
+        </Grid>) : undefined}
       </Container>
       </Grid>
       <Grid item xs={1} md={2}/>
